3fffc060842cd05d99ae9d4cca20e53ed0414baa
Anderson Bravalheri <andersonbravalheri@gmail.com>
2022-02-08 15:17:18 +0000

    Update to PyScaffold 4.1.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
     Setup file for pyscaffoldext-cookiecutter.
     Use setup.cfg to configure your project.
 
-    This file was generated with PyScaffold 4.0b3.
+    This file was generated with PyScaffold 4.1.4.
     PyScaffold helps you to put up the scaffold of your new Python project.
     Learn more under: https://pyscaffold.org/
 """
